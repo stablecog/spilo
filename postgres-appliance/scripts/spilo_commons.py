ba508d74ff62b9b46c1391b67d53c1121baa46c2
Brandon Berhent <bbedward@gmail.com>
2023-08-14 13:44:48 -0400

    add elsewhere

--- postgres-appliance/scripts/spilo_commons.py
+++ postgres-appliance/scripts/spilo_commons.py
@@ -13,6 +13,7 @@ LIB_DIR = '/usr/lib/postgresql'
 # (min_version, max_version, shared_preload_libraries, extwlist.extensions)
 extensions = {
     'timescaledb':    (9.6, 15, True,  True),
+    'pg_net': (12, 15, True, False),
     'pg_cron':        (9.5, 15, True,  False),
     'pg_stat_kcache': (9.4, 15, True,  False),
     'pg_partman':     (9.4, 15, False, True)
